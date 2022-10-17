375367ca315d50d92ac810f3410c974f062c804d
brunoais <brunoaiss@gmail.com>
2022-10-17 18:23:26 +0100

    tabs -> spaces

--- version/chrome/api.js
+++ version/chrome/api.js
@@ -4,33 +4,33 @@ export const VERSION = 'chrome';
 export const browser = globalThis.chrome;
 
 export function registerHlFilter(){
-	const lang = 'en-US';
-	browser.declarativeNetRequest.updateDynamicRules({
-		addRules: [
-			{
-				'id': 1,
-				'priority': 1,
-				'action': {
-					'type': 'redirect',
-					'redirect': {
-						'transform': {
-							'queryTransform': {
-								'addOrReplaceParams': [
-									{'key': 'hl', 'value': lang},
-								],
-							},
-						},
-					},
-				},
-				'condition': {
-					'regexFilter': '^https://[^\\.]*\\.(google|recaptcha)\\.(com|net)/recaptcha',
-					'resourceTypes': [
-						'sub_frame',
-						'script',
-					],
-				},
-			},
-		],
-		removeRuleIds: [1],
-	});
+    const lang = 'en-US';
+    browser.declarativeNetRequest.updateDynamicRules({
+        addRules: [
+            {
+                'id': 1,
+                'priority': 1,
+                'action': {
+                    'type': 'redirect',
+                    'redirect': {
+                        'transform': {
+                            'queryTransform': {
+                                'addOrReplaceParams': [
+                                    {'key': 'hl', 'value': lang},
+                                ],
+                            },
+                        },
+                    },
+                },
+                'condition': {
+                    'regexFilter': '^https://[^\\.]*\\.(google|recaptcha)\\.(com|net)/recaptcha',
+                    'resourceTypes': [
+                        'sub_frame',
+                        'script',
+                    ],
+                },
+            },
+        ],
+        removeRuleIds: [1],
+    });
 }
--- version/firefox/api.js
+++ version/firefox/api.js
@@ -4,15 +4,15 @@ export const VERSION = 'firefox';
 export const browser = globalThis.browser;
 
 export function registerHlFilter(){
-	const lang = 'en-US';
-	browser.webRequest.onBeforeSendHeaders.addListener(e => {
-		const url = new URL(e.url);
-		if (url.searchParams.get('hl') !== lang) {
-			url.searchParams.set('hl', lang);
-			return {redirectUrl: url.toString()};
-		}
-	}, {urls: [
-		'*://*.google.com/recaptcha/*',
-		'*://*.recaptcha.net/recaptcha/*',
-	], types: ['sub_frame']}, ['blocking']);
+    const lang = 'en-US';
+    browser.webRequest.onBeforeSendHeaders.addListener(e => {
+        const url = new URL(e.url);
+        if (url.searchParams.get('hl') !== lang) {
+            url.searchParams.set('hl', lang);
+            return {redirectUrl: url.toString()};
+        }
+    }, {urls: [
+        '*://*.google.com/recaptcha/*',
+        '*://*.recaptcha.net/recaptcha/*',
+    ], types: ['sub_frame']}, ['blocking']);
 }
